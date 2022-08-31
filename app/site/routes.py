9b68de38e6b9cdff8076a1634fe7f0ac6bfbf225
Antonio Popoca <104789557+albeoanguis1@users.noreply.github.com>
2022-08-30 20:51:37 -0400

    for the flask api 2 HW

--- app/site/routes.py
+++ app/site/routes.py
@@ -6,6 +6,6 @@ site = Blueprint('site', __name__, template_folder = 'site_templates')
 def home():
     return render_template('index.html')
 
-@site.route('/profile')
+@site.route('/garage')
 def profile():
     return render_template('profile.html')
